be4c5be6eb7596ab6af289fc6e00fb1508653c12
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-03-19 18:04:08 +0700

    add infinite scroll

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ทำเองใช้เองบนมือถือ <br>
-nekopost-add-style.user.js : เพิ่มstyle สำหรับตอนที่อ่านแล้ว + hover + กดซ่อนsidebar <br>
+nekopost-add-style.user.js : เพิ่มstyle สำหรับตอนที่อ่านแล้ว + hover + กดซ่อนsidebar + infinite scroll <br>
 nekopost-next-chapter.user.js : เลื่อนลงมาถึงตอนกดปุ่มแล้ว กดปุ่มให้เอง แล้วแก้ปุ่มcomment เป็น ปิด
--- nekopost-add-style.user.js
+++ nekopost-add-style.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nekopost-add-style
 // @namespace    https://github.com/Plong-Wasin
-// @version      0.2
+// @version      0.4
 // @description  nekopost-add-style
 // @author       Plong-Wasin
 // @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost-add-style.meta.js
@@ -16,5 +16,22 @@ $(document).ready(function() {
         "a[class='svelte-1wjyhvq']:hover,a[class='svelte-1wjyhvq']:visited{color: gray!important;}";
     $("li.svelte-qldq1i").on("click", function() {
         $("#kt_header_mobile_toggle").click();
+
+        $(".for-scroll.svelte-1xvrdhm").scroll(function() {
+            if (
+                $(".for-scroll.svelte-1xvrdhm").height() + 100 >=
+                $(".more.svelte-gbfxcs:last").offset().top
+            )
+                $(".more.svelte-gbfxcs:last").click();
+        });
     });
+    try {
+        $(".for-scroll.svelte-1xvrdhm").scroll(function() {
+            if (
+                $(".for-scroll.svelte-1xvrdhm").height() + 100 >=
+                $(".more.svelte-gbfxcs:last").offset().top
+            )
+                $(".more.svelte-gbfxcs:last").click();
+        });
+    } catch (error) {}
 });

9ab4ce12286846a91d53f9c287d534eb26fb9411
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-03-19 21:35:52 +0700

    :bug: Debug

--- nekopost-next-chapter.meta.js
+++ nekopost-next-chapter.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nekopost-next-chapter
 // @namespace    https://github.com/Plong-Wasin
-// @version      0.4
+// @version      0.5
 // @description  nekopost-next-chapter
 // @author       Plong-Wasin
 // @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost-next-chapter.meta.js

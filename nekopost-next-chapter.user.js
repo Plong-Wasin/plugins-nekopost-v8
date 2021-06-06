9a675ea0314e9da449d0c8ec5a1bb72fabc126e6
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-06-06 18:10:11 +0700

    Delete jquery

--- nekopost-next-chapter.user.js
+++ nekopost-next-chapter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nekopost-next-chapter
 // @namespace    https://github.com/Plong-Wasin
-// @version      0.5
+// @version      0.6
 // @description  nekopost-next-chapter
 // @author       Plong-Wasin
 // @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost-next-chapter.meta.js
@@ -11,7 +11,16 @@
 // ==/UserScript==
 let scrollCheck = false;
 let history = false;
-$(document).ready(function() {
+
+function ready(fn) {
+    if (document.readyState != "loading") {
+        fn();
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+ready(function() {
     window.addEventListener("pageshow", function(event) {
         var historyTraversal =
             event.persisted ||
@@ -23,16 +32,25 @@ $(document).ready(function() {
         }
     });
     if (!history) {
-        $(window).scroll(function() {
+        window.addEventListener("scroll", () => {
             if (
-                $(window).height() + $(window).scrollTop() + 200 >=
-                $("table:last").offset().top &&
+                window.innerHeight + window.screenY >=
+                document
+                .querySelectorAll("table")[
+                    document.querySelectorAll("table").length - 1
+                ].getBoundingClientRect().top &&
                 !scrollCheck &&
-                $("button:last").is(":enabled") &&
-                "Close" != $("button:last").html()
+                !document.querySelectorAll("button")[
+                    document.querySelectorAll("button").length - 1
+                ].disabled &&
+                "Close" !=
+                document.querySelectorAll("button")[
+                    document.querySelectorAll("button").length - 1
+                ].innerHTML
             ) {
                 scrollCheck = true;
-                $("button:last").click();
+                document
+                    .querySelectorAll("button")[document.querySelectorAll("button").length - 1].click();
             }
         });
     }
@@ -48,24 +66,27 @@ $(document).ready(function() {
         "fad fa-comments-alt"
     )[0].parentNode.innerHTML = "X";
     setTimeout(() => {
-        if ($("button:last").is(":disabled")) {
-            $("button:last").html("Close");
+        if (
+            document.getElementsByTagName("button")[
+                document.getElementsByTagName("button").length - 1
+            ].disabled
+        ) {
+            document.getElementsByTagName("button")[
+                document.getElementsByTagName("button").length - 1
+            ].innerHTML = "Close";
             document.getElementsByTagName("button")[
                 document.getElementsByTagName("button").length - 1
             ].disabled = false;
             document.getElementsByTagName("button")[
                 document.getElementsByTagName("button").length - 1
             ].style = "background-color: red!important;";
-            $("button:last").on("click", function() {
-                window.close();
-            });
+            document
+                .getElementsByTagName("button")[document.getElementsByTagName("button").length - 1].addEventListener(
+                    "click",
+                    () => {
+                        window.close();
+                    }
+                );
         }
     }, 2000);
-
-    // document.getElementsByClassName("btnComment")[0].style.backgroundColor =
-    //     "red";
-    // $(".btnComment").html("x");
-    // $(".btnComment").on("click", function() {
-    //     window.close();
-    // });
 });

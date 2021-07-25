5390551f6e41939dfadb548e8d9de74dbb8e5bcf
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-07-26 01:22:27 +0700

    load image

--- nekopost-next-chapter.user.js
+++ nekopost-next-chapter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         nekopost-next-chapter
 // @namespace    https://github.com/Plong-Wasin
-// @version      0.6
+// @version      0.7
 // @description  nekopost-next-chapter
 // @author       Plong-Wasin
 // @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost-next-chapter.meta.js
@@ -89,4 +89,50 @@ ready(function() {
                 );
         }
     }, 2000);
+    while (!checkLoadState);
+    insertImg();
 });
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function checkLoadState() {
+    if (document.images.length > 5) {
+        return true;
+    }
+    return false;
+    // await sleep(1000);
+    // checkLoadState();
+}
+
+function insertImg() {
+    let splitUrl = window.location.href.split("/");
+    let nc_chapter_id, np_project_id, nc_chapter_no;
+    np_project_id = splitUrl[splitUrl.length - 2];
+    nc_chapter_no = splitUrl[splitUrl.length - 1];
+    const d = new Date();
+    let date = d.getFullYear() + ((parseInt(d.getMonth()) + 1) >= 10 ? (parseInt(d.getMonth()) + 1) : '0' + (parseInt(d.getMonth()) + 1)) + ((parseInt(d.getDate())) >= 10 ? (parseInt(d.getDate())) : '0' + (parseInt(d.getDate()))) + ((parseInt(d.getHours())) >= 10 ? (parseInt(d.getHours())) : '0' + (parseInt(d.getHours()))) + ((parseInt(d.getMinutes())) >= 10 ? (parseInt(d.getMinutes())) : '0' + (parseInt(d.getMinutes())))
+    fetch(
+            `https://tuner.nekopost.net/ApiTest/getProjectDetailFull/${np_project_id}`
+        )
+        .then((response) => response.json())
+        .then((json) => {
+            for (item of json.projectChapterList) {
+                if (item.nc_chapter_no == nc_chapter_no) {
+                    nc_chapter_id = item.nc_chapter_id;
+                    break;
+                }
+            }
+            fetch(
+                    `https://fs.nekopost.net/collectManga/${np_project_id}/${nc_chapter_id}/${np_project_id}_${nc_chapter_id}.json?${date}`
+                )
+                .then((response2) => response2.json())
+                .then((json2) => {
+                    for (let i = 0; i < json2.pageItem.length; i++) {
+                        document.querySelectorAll('img.svelte-14e4tz0')[i].src = `https://fs.nekopost.net/collectManga/${np_project_id}/${nc_chapter_id}/${json2.pageItem[i].fileName}`
+                    }
+
+                });
+        });
+}

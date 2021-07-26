2767c65c153f2bac552779aaaabf54e8249796f4
Wasin Phungwigrai <wasin2541@hotmail.com>
2021-07-26 13:36:37 +0700

    change url

--- nekopost_infinite_scroll.meta.js
+++ nekopost_infinite_scroll.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         nekopost_infinity_scroll
+// @name         nekopost_infinite_scroll
 // @namespace    https://github.com/Plong-Wasin
 // @version      0.1
 // @description  nekopost-next-chapter
 // @author       Plong-Wasin
-// @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost_infinity_scroll.meta.js
-// @downloadURL  https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost_infinity_scroll.user.js
+// @updateURL    https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost_infinite_scroll.meta.js
+// @downloadURL  https://github.com/Plong-Wasin/plugins-nekopost-v8/raw/main/nekopost_infinite_scroll.user.js
 // @match        https://www.nekopost.net/manga/*/*
 // @grant        window.close

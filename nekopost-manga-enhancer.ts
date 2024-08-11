94067b14d2754b27992e121b33f2180e3dbbd719
Wasin Phungwigrai <wasin2541@hotmail.com>
2024-08-11 16:38:54 +0700

    🔥 feat(nekopost-manga-enhancer): add new script to enhance manga reading experience 🔥 feat(nekopost_infinite_scroll): remove infinite scroll functionality for now

---
+++ nekopost-manga-enhancer.ts
@@ -0,0 +1,144 @@
+(() => {
+  let isCheckedShowPageNo = false;
+  let isCheckedEagerLoad = false;
+  function ready(fn: () => void): void {
+    if (document.readyState != "loading") {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
+  function sleep(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+  /**
+   * Hides all page block elements initially, then displays them in reverse order.
+   * Ensures that each block containing an image is only displayed after the image source is loaded.
+   *
+   * @param pageBlockElements - Collection of HTML div elements representing page blocks.
+   */
+  async function processPageBlocks(
+    pageBlockElements: NodeListOf<HTMLDivElement>
+  ) {
+    // Convert the HTMLCollection to an array for easier manipulation
+    const pageBlockArray = Array.from(pageBlockElements);
+
+    // Hide all page block elements
+    pageBlockArray.forEach((blockElement) => {
+      blockElement.style.display = "none";
+    });
+
+    // Display each page block in reverse order, ensuring image sources are loaded
+    for (let i = pageBlockArray.length - 1; i >= 0; i--) {
+      const blockElement = pageBlockArray[i];
+      blockElement.style.display = "flex";
+      let j = 0;
+      //   Wait until an image within the block has its source loaded
+      while (j < 100) {
+        const imageElement = blockElement.querySelector("img");
+        if (imageElement?.src) {
+          break;
+        }
+        // Wait for 100 milliseconds before checking again
+        await sleep(10);
+        j++;
+      }
+    }
+  }
+  /**
+   * Hides all page block elements by setting their display style to "none".
+   *
+   * @param pageBlockElements - Collection of HTML div elements representing page blocks.
+   */
+  function hidePageBlockElements(
+    pageBlockElements: NodeListOf<HTMLDivElement>
+  ) {
+    // Convert the HTMLCollection to an array for easier iteration
+    const pageBlockArray = Array.from(pageBlockElements);
+
+    // Hide each page block element
+    pageBlockArray.forEach((blockElement) => {
+      blockElement.style.display = "none";
+    });
+
+    const toggleEls = document.querySelectorAll<HTMLDivElement>(
+      ".h-14.toggleTopMenu"
+    );
+
+    toggleEls.forEach((el) => {
+      el.style.display = "none";
+    });
+  }
+  function settingUI() {
+    const tableEl = document.querySelector("table");
+    const tbodyEl = tableEl?.querySelector("tbody");
+    const showPageNoCheckboxSettingValue =
+      localStorage.getItem("showPageNoCheckbox") ?? "true";
+    const eagerLoadCheckboxSettingValue =
+      localStorage.getItem("eagerLoadCheckbox") ?? "false";
+
+    const showPageNoSettingHtml = `<tr class="border-b"><td valign="top"><span class="font-semibold">Show Page No.</span></td> <td><div><input type="checkbox" id="showPageNoCheckbox" class="cursor-pointer"> <label for="showPageNoCheckbox" class="cursor-pointer">Show</label></div> </td></tr>`;
+    const eagerLoadSettingHtml = `<tr class="border-b"><td valign="top"><span class="font-semibold">Eager load</span></td> <td><div><input type="checkbox" id="eagerLoadCheckbox" class="cursor-pointer"> <label for="eagerLoadCheckbox" class="cursor-pointer">Enable</label></div> </td></tr>`;
+    tbodyEl?.insertAdjacentHTML("beforeend", showPageNoSettingHtml);
+    tbodyEl?.insertAdjacentHTML("beforeend", eagerLoadSettingHtml);
+    const showPageNoCheckbox = document.querySelector<HTMLInputElement>(
+      "#showPageNoCheckbox"
+    );
+    const eagerLoadCheckbox =
+      document.querySelector<HTMLInputElement>("#eagerLoadCheckbox");
+    if (showPageNoCheckboxSettingValue === "true") {
+      showPageNoCheckbox?.click();
+      isCheckedShowPageNo = true;
+    }
+    if (eagerLoadCheckboxSettingValue === "true") {
+      eagerLoadCheckbox?.click();
+      isCheckedEagerLoad = true;
+    }
+
+    showPageNoCheckbox?.addEventListener("change", () => {
+      if (showPageNoCheckbox.checked) {
+        localStorage.setItem("showPageNoCheckbox", "true");
+      } else {
+        localStorage.setItem("showPageNoCheckbox", "false");
+      }
+    });
+    eagerLoadCheckbox?.addEventListener("change", () => {
+      if (eagerLoadCheckbox.checked) {
+        localStorage.setItem("eagerLoadCheckbox", "true");
+      } else {
+        localStorage.setItem("eagerLoadCheckbox", "false");
+      }
+    });
+  }
+
+  ready(() => {
+    const mangaBlockEl = document.querySelector(
+      ".w-full.h-full.bg-gray-100.dark\\:bg-gray-800.min-h-screen"
+    );
+    settingUI();
+    const observer = new MutationObserver((mutations, observer) => {
+      if ((mangaBlockEl?.querySelectorAll("img")?.length ?? 0) > 2) {
+        observer.disconnect();
+        const pageNoBlockEls = document.querySelectorAll<HTMLDivElement>(
+          ".lg\\:inline-block.flex.flex-col.items-end"
+        );
+        const pageBlockEls = document.querySelectorAll<HTMLDivElement>(
+          ".flex.flex-col.lg\\:flex-row"
+        );
+        if (!isCheckedShowPageNo) {
+          hidePageBlockElements(pageNoBlockEls);
+        }
+        if (isCheckedEagerLoad) {
+          processPageBlocks(pageBlockEls);
+        }
+      }
+    });
+    if (mangaBlockEl) {
+      observer.observe(mangaBlockEl, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+      });
+    }
+  });
+})();
